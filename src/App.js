88d67cbb92ae446925ba9e8388e2bd751fa2d315
H-L <hadrien.lepoutre@gmail.com>
2019-02-27 18:07:32 +0100

    :sparkles: Adding react-router.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
-import TopBar from './components/TopBar';
+import StepOne from './components/steps/StepOne';
+import StepTwo from './components/steps/StepTwo';
+import StepThree from './components/steps/StepThree';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TopBar />
-        <Home />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path='/' component={Home}/>
+          <Route path='/step-one' component={StepOne}/>
+          <Route path='/step-two' component={StepTwo}/>
+          <Route path='/step-three' component={StepThree}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
